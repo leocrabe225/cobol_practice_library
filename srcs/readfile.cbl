3e83c590fd6cd4b5a1a30e98523aba56da186a0b
Leocrabe225 <petitscrounchs@yahoo.fr>
2025-06-03 17:04:49 +0200

    Actually opening file, and open error checking

--- srcs/readfile.cbl
+++ srcs/readfile.cbl
@@ -7,21 +7,27 @@
        ENVIRONMENT DIVISION.
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
-           SELECT OPTIONAL IN-FILE
+           SELECT OPTIONAL F-INPUT
                ASSIGN TO DISK
-               ORGANIZATION IS SEQUENTIAL
-               ACCESS MODE IS SEQUENTIAL
+               ORGANIZATION IS LINE SEQUENTIAL
                FILE STATUS IS WS-F-STATUS.
        DATA DIVISION.
        FILE SECTION.
-       FD IN-FILE
+       FD F-INPUT
            VALUE OF FILE-ID IS WS-FILE-NAME.
-       01 IN-LINE PIC X(20).
-           
+       01 F-IN-RCD.
+           05 F-IN-ISBN          PIC X(13).
+           05 F-IN-BOOK-NAME     PIC X(38).
+           05 F-IN-AUTH-NAME     PIC X(22).
+           05 F-IN-AUTH-FNAME    PIC X(22).
+           05 F-IN-TYPE          PIC X(16).
+           05 F-IN-YEAR          PIC 9(04).
+           05 F-IN-EDIT-NAME     PIC X(25).
 
        WORKING-STORAGE SECTION.
        01 WS-F-STATUS         PIC X(2).
            88 WS-F-STATUS-OK           VALUE '00'.
+           88 WS-F-STATUS-OPEN-ERROR   VALUE '05'.
            88 WS-F-STATUS-EOF          VALUE '10'.
            
        01 WS-FOLDER-NAME      PIC X(20) VALUE "input/".
@@ -31,14 +37,29 @@
        01 LK-FILE-NAME        PIC X(20).
 
        PROCEDURE DIVISION USING LK-FILE-NAME.
+
            STRING WS-FOLDER-NAME LK-FILE-NAME DELIMITED BY SPACE
            INTO WS-FILE-NAME.
-           DISPLAY WS-FILE-NAME.
            
-           OPEN INPUT IN-FILE.
-           READ IN-FILE.
-           DISPLAY IN-LINE.
-           CLOSE IN-FILE.
+           OPEN INPUT F-INPUT.
+
+           IF WS-F-STATUS-OPEN-ERROR THEN
+               DISPLAY "Error, file " QUOTES FUNCTION TRIM(WS-FILE-NAME)
+                       QUOTES " does not exist"
+               EXIT PROGRAM
+           END-IF.
+
+           PERFORM UNTIL WS-F-STATUS-EOF
+               READ F-INPUT
+                   NOT AT END
+                       PERFORM NO-OP
+               END-READ
+           END-PERFORM
+
+           CLOSE F-INPUT.
            
 
            EXIT PROGRAM.
+
+       NO-OP.
+           .
