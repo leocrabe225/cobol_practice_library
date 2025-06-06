dd2b33bffb9bb85d53c4f62fa4dd5082b5a08ff3
Leocrabe225 <petitscrounchs@yahoo.fr>
2025-06-06 07:35:07 +0200

    Books read and create almost finished, error -400 for now

--- srcs/books_crud/creabook.cbl
+++ srcs/books_crud/creabook.cbl
@@ -6,60 +6,87 @@
 
        DATA DIVISION. 
 
-
        WORKING-STORAGE SECTION.
        EXEC SQL BEGIN DECLARE SECTION END-EXEC.
 
-       01  WS-BOOK-NAME   PIC X(50).
+       01 WS-ISBN         PIC X(13).
+       01 WS-NAME         PIC X(50).
+       01 WS-YEAR         PIC X(04).
+       01 WS-AUTHOR-ID    PIC 9(10).
+       01 WS-AUTHOR-NAME  PIC X(25).
+       01 WS-AUTHOR-FNAME PIC X(25).
+       01 WS-EDITOR-ID    PIC 9(10).
+       01 WS-EDITOR-NAME  PIC X(25).
+       01 WS-TYPE-ID      PIC 9(10).
+       01 WS-TYPE-NAME    PIC X(20).
 
        EXEC SQL END DECLARE SECTION END-EXEC.
 
+       01 WS-DULL         PIC X(50).
+
        EXEC SQL INCLUDE SQLCA END-EXEC.
 
-       LINKAGE SECTION. 
-       01  LK-TYPE-NAME   PIC X(50).
+       LINKAGE SECTION.
+       01 LK-ISBN        PIC X(13).
+       01 LK-NAME        PIC X(50).
+       01 LK-YEAR        PIC X(04).
+       01 LK-AUTHOR-ID   PIC 9(10).
+       01 LK-EDITOR-ID   PIC 9(10).
+       01 LK-TYPE-ID     PIC 9(10).
+       COPY retstatu REPLACING ==:PREFIX:== BY ==LK==.
         
 
-       PROCEDURE DIVISION USING LK-TYPE-NAME. 
+       PROCEDURE DIVISION USING LK-ISBN,
+                                LK-NAME,
+                                LK-YEAR,
+                                LK-AUTHOR-ID,
+                                LK-EDITOR-ID,
+                                LK-TYPE-ID,
+                                LK-RETURN-VALUE. 
        
-       MOVE LK-TYPE-NAME TO WS-BOOK-NAME.
+           MOVE LK-ISBN      TO WS-ISBN.
+           MOVE LK-NAME      TO WS-NAME.
+           MOVE LK-YEAR      TO WS-YEAR.
+           MOVE LK-AUTHOR-ID TO WS-AUTHOR-ID.
+           MOVE LK-EDITOR-ID TO WS-EDITOR-ID.
+           MOVE LK-TYPE-ID   TO WS-TYPE-ID.
 
        
-       EXEC SQL 
-          SELECT name 
-          INTO :WS-BOOK-NAME,  
-          FROM books
-          WHERE name = :WS-BOOK-NAME 
-       END-EXEC.
+           CALL "readbook" USING
+               WS-NAME
+               WS-DULL
+               WS-DULL
+               WS-DULL
+               WS-DULL
+               WS-DULL
+               WS-DULL
+               WS-DULL
+               LK-RETURN-VALUE
+           END-CALL.
        
-       EVALUATE SQLCODE 
-           
-           WHEN +100
-              EXEC SQL
-              INSERT INTO books (name)
-              VALUES (:WS-BOOK-NAME)
-              END-EXEC
-              EXEC SQL COMMIT END-EXEC
-           
-           WHEN 0
-              DISPLAY "This book is already in the database."
+           EVALUATE TRUE
+               WHEN LK-RETURN-OK
+                   SET LK-RETURN-ALREADY-HERE TO TRUE
+                   EXIT PROGRAM
+               WHEN LK-RETURN-ERROR
+                   EXIT PROGRAM
+           END-EVALUATE.
 
-       END-EVALUATE.
-
-
-       IF SQLCODE = 0
-          DISPLAY "Insertion successful."
-
-       ELSE
-          DISPLAY "Insertion error SQLCODE: " SQLCODE
-          EXEC SQL 
-           ROLLBACK 
-          END-EXEC 
-
-       END-IF.
-
-
-       MOVE WS-BOOK-NAME TO LK-TYPE-NAME.
-
-       EXIT PROGRAM.
+       EXEC SQL
+           INSERT INTO books (isbn, name, year, author_id, editor_id,
+               type_id)
+           VALUES (:WS-ISBN, :WS-NAME, :WS-YEAR, WS-AUTHOR-ID,
+               WS-EDITOR-ID, WS-TYPE-ID)
+       END-EXEC
+      
+           EVALUATE SQLCODE
+               WHEN 0
+       EXEC SQL COMMIT END-EXEC
+                   SET LK-RETURN-OK TO TRUE
+               WHEN OTHER
+                   SET LK-RETURN-ERROR TO TRUE
+       EXEC SQL ROLLBACK END-EXEC
+           END-EVALUATE.
+      
+           EXIT PROGRAM.
        
--- srcs/books_crud/menubook.cbl
+++ srcs/books_crud/menubook.cbl
@@ -10,9 +10,12 @@
 
        COPY crudstr.
        COPY retstatu REPLACING ==:PREFIX:== BY ==WS==.
-       
-       01  WS-BOOK-NAME         PIC X(50).
+
        01  WS-BOOK-ID           PIC 9(10).
+       01  WS-BOOK-NAME         PIC X(50).
+      
+       01  WS-BOOK-ISBN         PIC X(13).
+       01  WS-BOOK-YEAR         PIC 9(04).
 
        01  WS-AUTHOR-LASTNAME   PIC X(25).
        01  WS-AUTHOR-FIRSTNAME  PIC X(25).
@@ -21,9 +24,8 @@
        01  WS-TYPE-NAME         PIC X(20).
        01  WS-TYPE-ID           PIC 9(10).
        
-       01 WS-NAME               PIC X(25).
-       01 WS-ID                 PIC 9(10).
-
+       01  WS-EDITOR-NAME               PIC X(25).
+       01  WS-EDITOR-ID                 PIC 9(10).
 
        01 WS-QUIT      PIC X   VALUE "n".
            88 WS-QUIT-N        VALUE "n".
@@ -34,6 +36,57 @@
        01 WS-USER-RESPONSE         PIC X.
            88 WS-USER-RESPONSE-N           VALUE "n".
            88 WS-USER-RESPONSE-Y           VALUE "Y".
+
+       01 WS-OUT-HDR.
+           05 FILLER          PIC X(10) VALUE "id".
+           05 FILLER          PIC X(03) VALUE " | ".
+           05 FILLER          PIC X(13) VALUE "isbn".
+           05 FILLER          PIC X(03) VALUE " | ".
+           05 FILLER          PIC X(25) VALUE "book name".
+           05 FILLER          PIC X(03) VALUE " | ".
+           05 FILLER          PIC X(04) VALUE "year".
+           05 FILLER          PIC X(03) VALUE " | ".
+           05 FILLER          PIC X(25) VALUE "author first_name".
+           05 FILLER          PIC X(03) VALUE " | ".
+           05 FILLER          PIC X(25) VALUE "author last_name".
+           05 FILLER          PIC X(03) VALUE " | ".
+           05 FILLER          PIC X(25) VALUE "editor name".
+           05 FILLER          PIC X(03) VALUE " | ".
+           05 FILLER          PIC X(25) VALUE "type name".
+
+       01 WS-OUT-SEPARATION-LINE.
+           05 FILLER          PIC X(10) VALUE ALL "-".
+           05 FILLER          PIC X(03) VALUE "-*-".
+           05 FILLER          PIC X(13) VALUE ALL "-".
+           05 FILLER          PIC X(03) VALUE "-*-".
+           05 FILLER          PIC X(25) VALUE ALL "-".
+           05 FILLER          PIC X(03) VALUE "-*-".
+           05 FILLER          PIC X(04) VALUE ALL "-".
+           05 FILLER          PIC X(03) VALUE "-*-".
+           05 FILLER          PIC X(25) VALUE ALL "-".
+           05 FILLER          PIC X(03) VALUE "-*-".
+           05 FILLER          PIC X(25) VALUE ALL "-".
+           05 FILLER          PIC X(03) VALUE "-*-".
+           05 FILLER          PIC X(25) VALUE ALL "-".
+           05 FILLER          PIC X(03) VALUE "-*-".
+           05 FILLER          PIC X(25) VALUE ALL "-".
+
+       01 WS-OUT-LINE.
+           05 WS-OUT-BOOK-ID       PIC X(10).
+           05 FILLER               PIC X(03) VALUE " | ".
+           05 WS-OUT-BOOK-ISBN     PIC X(13).
+           05 FILLER               PIC X(03) VALUE " | ".
+           05 WS-OUT-BOOK-NAME     PIC X(25).
+           05 FILLER               PIC X(03) VALUE " | ".
+           05 WS-OUT-YEAR          PIC 9(04).
+           05 FILLER               PIC X(03) VALUE " | ".
+           05 WS-OUT-AUTHOR-FNAME  PIC X(25).
+           05 FILLER               PIC X(03) VALUE " | ".
+           05 WS-OUT-AUTHOR-NAME   PIC X(25).
+           05 FILLER               PIC X(03) VALUE " | ".
+           05 WS-OUT-EDITOR-NAME   PIC X(25).
+           05 FILLER               PIC X(03) VALUE " | ".
+           05 WS-OUT-TYPE-NAME     PIC X(25).
 
        PROCEDURE DIVISION.
        
@@ -56,10 +109,10 @@
                WHEN WS-CREATE-STRING
                    DISPLAY "Enter book's name : "
                    ACCEPT WS-BOOK-NAME
-
-                   CALL "creabook" USING 
-                        WS-BOOK-NAME
-                   END-CALL
+                   DISPLAY "Enter book's isbn : "
+                   ACCEPT WS-BOOK-ISBN
+                   DISPLAY "Enter book's release year : "
+                   ACCEPT WS-BOOK-YEAR
                    
                    DISPLAY "Enter author's lastname : "
                    ACCEPT WS-AUTHOR-LASTNAME
@@ -88,6 +141,12 @@
 
                                IF  WS-RETURN-OK
                                DISPLAY "Author added successfully"
+                                   CALL "readauth" USING 
+                                        WS-AUTHOR-LASTNAME 
+                                        WS-AUTHOR-FIRSTNAME
+                                        WS-AUTHOR-ID
+                                        WS-RETURN-VALUE
+                                   END-CALL 
                                END-IF 
 
                            ELSE 
@@ -128,6 +187,11 @@
 
                                IF  WS-RETURN-OK
                                 DISPLAY "Book type added successfully"
+                                   CALL "readtype" USING 
+                                       WS-TYPE-NAME
+                                       WS-TYPE-ID
+                                       WS-RETURN-VALUE
+                                   END-CALL 
                                END-IF 
 
                            ELSE 
@@ -143,10 +207,10 @@
                    END-EVALUATE
 
                    DISPLAY "Enter editor's name : "
-                   ACCEPT WS-NAME 
+                   ACCEPT WS-EDITOR-NAME 
                    CALL "readedit" USING 
-                        WS-NAME 
-                        WS-ID
+                        WS-EDITOR-NAME 
+                        WS-EDITOR-ID
                         WS-RETURN-VALUE
                    END-CALL 
 
@@ -158,12 +222,17 @@
        
                            IF WS-USER-RESPONSE-Y
                                CALL "creaedit" USING
-                                     WS-NAME
+                                     WS-EDITOR-NAME
                                      WS-RETURN-VALUE
                                END-CALL 
                                
                                IF  WS-RETURN-OK
                                DISPLAY "Editor added successfully"
+                                   CALL "readedit" USING 
+                                       WS-EDITOR-NAME 
+                                       WS-EDITOR-ID
+                                       WS-RETURN-VALUE
+                                   END-CALL 
                                END-IF 
 
                            ELSE 
@@ -178,14 +247,43 @@
                            DISPLAY "Editor Read/Create error."
                    END-EVALUATE
 
-      *        WHEN WS-READ-STRING
-      *            DISPLAY "Enter book's id: "
-      *            ACCEPT WS-BOOK-ID
-      *
-      *            CALL "readbook" USING 
-      *                 WS-BOOK-ID
-      *                 WS-BOOK-NAME
-      *            END-CALL 
+                   CALL "creabook" USING
+                       WS-BOOK-ISBN
+                       WS-BOOK-NAME
+                       WS-BOOK-YEAR
+                       WS-AUTHOR-ID
+                       WS-EDITOR-ID
+                       WS-TYPE-ID
+                       WS-RETURN-VALUE
+                   END-CALL
+
+               WHEN WS-READ-STRING
+                   DISPLAY "Enter book's name: "
+                   ACCEPT WS-BOOK-NAME
+       
+                   CALL "readbook" USING 
+                        WS-BOOK-NAME
+                        WS-BOOK-ID
+                        WS-BOOK-ISBN,
+                        WS-BOOK-YEAR,
+                        WS-AUTHOR-LASTNAME,
+                        WS-AUTHOR-FIRSTNAME,
+                        WS-EDITOR-NAME,
+                        WS-TYPE-NAME,
+                        WS-RETURN-VALUE
+                   END-CALL 
+
+                   EVALUATE TRUE
+                       WHEN WS-RETURN-OK
+                           PERFORM 0500-MOVE-BOOK-TO-OUT-LINE-BEGIN
+                              THRU 0500-MOVE-BOOK-TO-OUT-LINE-END
+                           PERFORM 0600-DISPLAY-OUT-LINE-BEGIN
+                              THRU 0600-DISPLAY-OUT-LINE-END
+                       WHEN WS-RETURN-NOT-FOUND
+                           DISPLAY "Record not found"
+                       WHEN WS-RETURN-ERROR
+                           DISPLAY "Book read error"
+                   END-EVALUATE
       *
       *        WHEN WS-UPDATE-STRING
       *            DISPLAY "Enter book's id: "
@@ -216,4 +314,22 @@
        END-PERFORM.
 
        EXIT PROGRAM.
+
+       0500-MOVE-BOOK-TO-OUT-LINE-BEGIN.
+           MOVE WS-BOOK-ID          TO WS-OUT-BOOK-ID.
+           MOVE WS-BOOK-ISBN        TO WS-OUT-BOOK-ISBN.
+           MOVE WS-BOOK-NAME        TO WS-OUT-BOOK-NAME.
+           MOVE WS-BOOK-YEAR        TO WS-OUT-YEAR.
+           MOVE WS-AUTHOR-FIRSTNAME TO WS-OUT-AUTHOR-FNAME.
+           MOVE WS-AUTHOR-LASTNAME  TO WS-OUT-AUTHOR-NAME.
+           MOVE WS-EDITOR-NAME      TO WS-OUT-EDITOR-NAME.
+           MOVE WS-TYPE-NAME        TO WS-OUT-TYPE-NAME.
+       0500-MOVE-BOOK-TO-OUT-LINE-END.
        
+       0600-DISPLAY-OUT-LINE-BEGIN.
+           DISPLAY WS-OUT-SEPARATION-LINE.
+           DISPLAY WS-OUT-HDR.
+           DISPLAY WS-OUT-SEPARATION-LINE.
+           DISPLAY WS-OUT-LINE.
+           DISPLAY WS-OUT-SEPARATION-LINE.
+       0600-DISPLAY-OUT-LINE-END.
